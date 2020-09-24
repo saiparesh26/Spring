a8ca0bdd119e8b32818f7fa92370a4ff5f5073c3
Sai Paresh <saiparesh26@gmail.com>
2020-09-24 10:34:15 +0530

    demo

---
+++ demo/src/main/java/com/sp/web/Product.java
@@ -0,0 +1,39 @@
+package com.sp.web;
+
+public class Product {
+	private int id;
+	private String name;
+	private double price;
+	
+	public Product() {
+		
+	}
+	
+	public Product(int id, String name, double price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public int getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public double getPrice() {
+		return price;
+	}
+	
+	
+}
---
+++ demo/src/main/java/com/sp/web/UserController.java
@@ -0,0 +1,39 @@
+package com.sp.web;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+	
+	@RequestMapping("/{userId}")
+	public String displayUser(@PathVariable int userId) {
+		return "User found: " + userId;
+	}
+	
+	@RequestMapping("/{id}/invoices")
+	public String displayUserInvoices(@PathVariable("id") int userId, 
+			@RequestParam(value = "date", required = false) Date dateOrNull) {
+		return "Invoice found for the user: " + userId + " on the date: " + dateOrNull;
+	}
+	
+	@RequestMapping("/{userId}/items")
+	public List<String> displayStringJson() {
+		return Arrays.asList("Shows", "Laptop", "button");
+	}
+	
+	@RequestMapping("/{userId}/products_as_json")
+	public List<Product> displayProductsJson() {
+		return Arrays.asList(new Product(1, "Shows", 200), 
+							 new Product(2, "Watch", 500), 
+						     new Product(3, "Gloves", 300));
+		
+	}
+}
